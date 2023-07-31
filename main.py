affab8c071abaee8344d81d308725b7a0736f63f
Semih Orhan <sorhan3.14@gmail.com>
2023-07-31 12:43:56 +0300

    implement path parameters

--- main.py
+++ main.py
@@ -9,5 +9,15 @@ def index():
     return {"message": "hello semih"}
 
 
+@app.get("/blog/all")
+def get_all_blogs():
+    return {"data": "all blogs"}
+
+
+@app.get("/blog/{id}")
+def get_blog(id: int):
+    return {"data": f"Blog with id {id}"}
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
