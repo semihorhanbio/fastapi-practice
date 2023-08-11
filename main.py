494b801a75ac228af3387972025c190dfd16896d
Semih Orhan <sorhan3.14@gmail.com>
2023-08-11 11:23:45 +0300

    add description to functions

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ def index():
     return {"message": "hello semih"}
 
 
-@app.get("/blog/all", tags=["blog"])
+@app.get(
+    "/blog/all",
+    tags=["blog"],
+    summary="Retrieve all blogs",
+    description="This api call simulates fetching all blogs",
+)
 def get_all_blogs(page=1, page_size: Optional[int] = None):
     return {"message": f"All {page_size} blogs on page {page}"}
 
@@ -20,6 +25,13 @@ def get_all_blogs(page=1, page_size: Optional[int] = None):
 def get_comment(
     id: int, comment_id: int, valid: bool = True, username: Optional[str] = None
 ):
+    """
+    Simulates retrieving a comment of a blog
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **bool** optional query parameter
+    - **username** optional query parameter
+    """
     return {
         "message": f"Blog with id {id} and comment id {comment_id} and valid {valid} and username {username}"
     }
@@ -33,11 +45,19 @@ class BlogType(str, Enum):
 
 @app.get("/blog/type/{type}", tags=["blog"])
 def get_blog_type(type: BlogType):
+    """
+    Simulates retrieving a blog type
+    - **type** mandatory path parameter
+    """
     return {"data": f"Blog type {type.name}"}
 
 
 @app.get("/blog/{id}", tags=["blog"])
 def get_blog(id: int, response: Response):
+    """
+    Simulates retrieving a blog
+    - **id** mandatory path parameter
+    """
     if id > 5:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"data": f"Blog with id {id} not found"}
