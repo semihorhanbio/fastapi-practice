5e63b85c16149cd3988bd85edeabce7037cb26d5
Semih Orhan <sorhan3.14@gmail.com>
2023-08-20 16:00:33 +0300

    practice with generators

---
+++ python-practice/python-programming-basics/generators.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_generator = (number for number in numbers if number % 2 == 0)
+even = list(even_generator)
+even_bis = list(even_generator)
+
+print(even)
+print(even_bis)
+
+
+def get_even_numbers(max):
+    for i in range(2, max + 1):
+        if i % 2 == 0:
+            yield i
+    print("Generator exhausted")
+
+
+even = list(get_even_numbers(10))
+print(even)
