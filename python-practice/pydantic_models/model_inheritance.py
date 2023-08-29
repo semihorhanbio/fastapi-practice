256b138b70e93f2f98b6f66d096b71a1ea0a60e7
Semih Orhan <sorhan3.14@gmail.com>
2023-08-29 09:15:43 +0300

    model inheritance in pydantic

---
+++ python-practice/pydantic_models/model_inheritance.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+
+    def excerpt(self) -> str:
+        return f"{self.content[:140]}..."
+
+
+class PostRead(PostBase):
+    id: int
+
+
+class Post(PostBase):
+    id: int
+    nb_views: int = 0
